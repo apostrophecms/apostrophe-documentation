40e663671ba19835a5ce8faf2c2afaac858b5c36
Kyle Stetz <kyle@punkave.com>
2014-06-20 14:59:53 -0400

    refactored styles for maximum enjoyment

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var h1 = document.querySelectorAll('h1');
-  if(h1[0].innerText.length > 30) {
-    h1[0].classList.add('long');
-  }
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var h1 = document.querySelectorAll('h1');
+//   if(h1[0].innerText.length > 30) {
+//     h1[0].classList.add('long');
+//   }
+// });
 
 // Not yet
 // $(function() {
