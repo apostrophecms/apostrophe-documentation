9b443854399aa92502c1e0ee35d14dcbf673817c
Tom Boutell <tom@punkave.com>
2014-10-08 08:53:17 -0400

    holycrap we have sweet documentation for all our nunjucks functions and you can find it

--- javascript/main.js
+++ javascript/main.js
@@ -13,39 +13,40 @@ $( function() {
 //   }
 // });
 
-// Not yet
-// $(function() {
-//   // Turn internal headings into an additional sidebar section
-//   // Grab the ones that markdown gave IDs
-//   var $headings = $('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]');
-//   var $sidebar = $('.sidebar');
-//   var links = [];
-//   $headings.each(function() {
-//     var $heading = $(this);
-//     var text = $heading.text();
-//     var name = $heading.attr('id');
-//     links.push({ text: text, name: name });
-//   });
-//   if (links.length) {
-//     var $ul = $('<ul class="internal-links"></ul>');
-//     $.each(links, function(i, link) {
-//       var $li = $('<li></li>');
-//       var $link = $('<a></a>');
-//       $link.attr('href', '#' + link.name);
-//       $link.attr('data-name', link.name);
-//       $link.text(link.text);
-//       $li.append($link);
-//       $ul.append($li);
-//     });
-//     $sidebar.append($ul);
-//   }
-//   // marked doesn't produce real named anchors by default,
-//   // so implement our own pretty scroll
-//   $('.sidebar a[data-name]').on('click', function() {
-//     var name = $(this).attr('data-name');
-//     $('html, body').animate({
-//       scrollTop: $("#" + name).offset().top - 50
-//     }, 500);
-//     return false;
-//   });
-// });
+$(function() {
+  // Turn internal headings into an additional sidebar section
+  // Grab the ones that markdown gave anchors
+  var $headings = $('h2:has(a[name]),h3:has(a[name])');
+  var $sidebar = $('.sidebar');
+  var links = [];
+  var seen = {};
+  $headings.each(function() {
+    var $heading = $(this);
+    var text = $heading.text();
+    // Don't show the parens of a function or link
+    // to all the syntaxes, just the name is enough
+    text = text.replace(/\(.*$/, '');
+    if (seen[text]) {
+      return;
+    }
+    seen[text] = true;
+    var name = $heading.find('a').attr('name');
+    var level = $heading.prop('tagName');
+    level = level.replace(/h/i, '');
+    links.push({ text: text, name: name, level: level });
+  });
+  if (links.length) {
+    var $ul = $('<ul class="internal-links"></ul>');
+    $.each(links, function(i, link) {
+      var $li = $('<li></li>');
+      $li.addClass('h' + link.level);
+      var $link = $('<a></a>');
+      $link.attr('href', '#' + link.name);
+      $link.attr('data-name', link.name);
+      $link.text(link.text);
+      $li.append($link);
+      $ul.append($li);
+    });
+    $sidebar.append($ul);
+  }
+});
