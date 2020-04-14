7d309d744388b4a55170028f5222b9fd929fd179
Alex Bea <abea@mailbox.org>
2020-04-14 12:54:19 -0500

    adds a todo

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,6 @@
 export default ({ router }) => {
   router.addRoutes([
+    // TODO: set up redirects
     // '/apostrophe-devops/devops/*' to '/apostrophe-devops/*',
     // 'howtos/howtos' to 'howtos',
     // '/howtos/deployment/*' to '/apostrophe-devops/deployment/*',
