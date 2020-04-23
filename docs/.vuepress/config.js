3b5c0c7e3317f34ff86c08f38c2e6f8bd8eb4841
Alex Bea <abea@mailbox.org>
2020-04-23 12:14:31 -0500

    Adds docs main nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,28 @@ module.exports = {
     lastUpdated: 'Last updated',
     nextLinks: true,
     prevLinks: true,
-    sidebar
+    sidebar,
+    logo: '/images/a2-lockup.png',
+    nav: [
+      {
+        text: 'Docs Home',
+        link: '/'
+      },
+      {
+        text: 'Community',
+        link: 'https://apostrophecms.com/community',
+        rel: false
+      },
+      {
+        text: 'Demo',
+        link: 'http://demo.apostrophecms.org',
+        rel: false
+      },
+      {
+        text: 'Main Site',
+        link: 'https://apostrophecms.com',
+        rel: false
+      }
+    ]
   }
 };
